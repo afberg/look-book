2634dc7f805954cfd23ee7d8f667324363e1202c
Anton Forsberg <a.forsberg.91@gmail.com>
2019-06-11 18:49:16 +0200

    Adds test for search with empty base string

--- src/services/search.test.ts
+++ src/services/search.test.ts
@@ -3,4 +3,9 @@ import { buildSearchUrl } from "./search";
 test("Should handle spaces properly", () => {
     const lotr = buildSearchUrl("lotr", "lord of the rings")
     expect(lotr).toBe("lotr?q=lord+of+the+rings");
+});
+
+test("Should handle being given no base", () => {
+    const lotr = buildSearchUrl("", "lord of the rings")
+    expect(lotr).toBe("?q=lord+of+the+rings");
 })
